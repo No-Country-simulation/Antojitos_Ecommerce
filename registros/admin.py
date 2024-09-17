990d50e75525b5a824b4a892a8e273e43a6e53bf
Lucas Callamullo <lucascallamullo@hotmail.com>
2024-09-17 10:47:13 -0300

    Update Carousel

--- registros/admin.py
+++ registros/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from registros.models import SellerUser
+
+
+
 # Register your models here.
+class SellerUserAdmin(admin.ModelAdmin):
+    pass
+    # establece estos campos solo como lectura
+    # readonly_fields = ('created', 'updated')
+    
+    
+admin.site.register(SellerUser, SellerUserAdmin)
