887ea37ddb5527332422a2f8325ed582b5069f13
US <laylamelanygomez@gmail.com>
2024-09-19 16:12:07 +0200

    update setting debug

--- django_comida_crud_api/settings.py
+++ django_comida_crud_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rl_%0_ri@)+6-!4jw+c50u*sb9@xi04x2+gz_rtt3b4i3oed8q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
